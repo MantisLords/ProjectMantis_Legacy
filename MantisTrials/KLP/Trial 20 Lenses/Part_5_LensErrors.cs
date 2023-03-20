0a09577935842e0f5ba6901ab164f1a026c6ebaa
ClebschGordon <salahudin.smailagic@gmail.com>
2023-03-20 17:13:56 +0100

    .

---
+++ MantisTrials/KLP/Trial 20 Lenses/Part_5_LensErrors.cs
@@ -0,0 +1,15 @@
+using Mantis.DocumentEngine;
+using Mantis.DocumentEngine.TableCreator;
+
+namespace MantisTrials.KLP.Trial_20_Lenses;
+
+public class Part_5_LensErrors
+{
+    private static MantisDocument CurrentDocument => Main_Trial_20_Lenses.CurrentDocument;
+    private static TableCreator CurrentTableCreator => Main_Trial_20_Lenses.CurrentTableCreator;
+
+    public static void Generate()
+    {
+        
+    }
+}
